b3a484945d34f30ff01f216ebf507746295e3bc1
hidapen <hidapen0630@gmail.com>
2021-06-11 09:35:26 +0900

    ユーザー認証機能の実装(再追加)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:/]
+  # only: [:/]を追記したことで、ログアウト後にトップページへ遷移できた。それまではログアウト後、ログインページへ遷移する状態だった。
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
