53ac730e02aaeb1a338947e644f964acb6841160
hidapen <hidapen0630@gmail.com>
2021-06-16 11:40:42 +0900

    レビュー修正２回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,54 +29,29 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("商品の説明を入力してください")
       end
       it 'カテゴリー情報がなければ保存できないこと' do
-        @item.category_id = nil
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("カテゴリーを入力してください")
-      end
-      it 'カテゴリー情報で初期値の---を選択したときは保存ができないこと' do
-        @item.category_id = '---'
-        @item.valid?
-        expect(@item.errors.full_messages).to include("カテゴリーは数値で入力してください")
+        expect(@item.errors.full_messages).to include("カテゴリーは1以外の値にしてください")
       end
       it '商品の状態の情報がなければ保存されないこと' do
-        @item.item_status_id = nil
+        @item.item_status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品の状態を入力してください")
-      end
-      it '商品の状態の情報で初期値の---を選択したときは保存ができないこと' do
-        @item.item_status_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("商品の状態は数値で入力してください")
+        expect(@item.errors.full_messages).to include("商品の状態は1以外の値にしてください")
       end
       it '配送料の負担の情報がなければ保存されないこと' do
-        @item.delivery_charge_id = nil
+        @item.delivery_charge_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("配送料の負担を入力してください")
-      end
-      it '配送料の負担の情報で初期値の---を選択したときは保存ができないこと' do
-        @item.delivery_charge_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("配送料の負担は数値で入力してください")
+        expect(@item.errors.full_messages).to include("配送料の負担は1以外の値にしてください")
       end
       it '発送元の地域の情報がなければ保存できないこと' do
-        @item.prefecture_id = nil
+        @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("発送元の地域を入力してください")
-      end
-      it '発送元の地域の情報で初期値の---を選択したときは保存ができないこと' do
-        @item.prefecture_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("発送元の地域は数値で入力してください")
+        expect(@item.errors.full_messages).to include("発送元の地域は1以外の値にしてください")
       end
       it '発送までの日数の情報がなければ保存できないこと' do
-        @item.delivery_date_id = nil
+        @item.delivery_date_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("発送までの日数を入力してください")
-      end
-      it '発送までの日数の情報で初期値の---を選択したときは保存ができないこと' do
-        @item.delivery_date_id = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("発送までの日数は数値で入力してください")
+        expect(@item.errors.full_messages).to include("発送までの日数は1以外の値にしてください")
       end
       it '価格の情報がなければ保存できないこと' do
         @item.cost = nil
