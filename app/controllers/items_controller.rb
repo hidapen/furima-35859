2d495c4b2b1ce54d248482b076485528ee27c76e
hidapen <hidapen0630@gmail.com>
2021-06-17 06:16:48 +0900

    レビュー修正３回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_tweet, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -23,13 +24,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user_id == current_user.id
-      redirect_to action: :index
-    end
   end
 
   def update
-    if @item.update(item_params) && @item.user_id == current_user.id
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
@@ -43,7 +41,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image,:title,:description,:category_id,:item_status_id,:delivery_charge_id,:prefecture_id,:delivery_date_id,:cost).merge(user_id: current_user.id)
   end
 
-  def set_tweet
+  def set_item
     @item = Item.find(params[:id])
   end
+
+  def move_to_index
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
+end
 end
