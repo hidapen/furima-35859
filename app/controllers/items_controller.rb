ab2c3faa890172b0e57b35de9dc97e80815a387a
hidapen <hidapen0630@gmail.com>
2021-06-16 20:15:38 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:/, :new, :create]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,11 +23,33 @@ class ItemsController < ApplicationController
     @items = Item.find(params[:id])
   end
 
+  def edit
+    @items = Item.find(params[:id])
+    unless @items.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @items = Item.find(params[:id])
+    if @items.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   
   private
 
   def item_params
     params.require(:item).permit(:image,:title,:description,:category_id,:item_status_id,:delivery_charge_id,:prefecture_id,:delivery_date_id,:cost).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+        redirect_to new_user_session_path
+    end
   end
 
 end
