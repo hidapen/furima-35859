d985d668c6ab91896411e4a61bba16907b79620a
hidapen <hidapen0630@gmail.com>
2021-06-22 13:01:05 +0900

    マイグレーションファイル内の整理

--- db/migrate/20210614084333_create_items.rb
+++ db/migrate/20210614084333_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string     :title,              null: false
-      t.text       :description,        null: false
-      t.integer    :category_id,        null: false
-      t.integer    :item_status_id,     null: false
-      t.integer    :delivery_charge_id, null: false
-      t.integer    :prefecture_id,      null: false
-      t.integer    :delivery_date_id,   null: false
-      t.integer    :cost,               null: false
-      t.references :user,               null: false, foreign_key: true
+      t.string     :title              ,null: false
+      t.text       :description        ,null: false
+      t.integer    :category_id        ,null: false
+      t.integer    :item_status_id     ,null: false
+      t.integer    :delivery_charge_id ,null: false
+      t.integer    :prefecture_id      ,null: false
+      t.integer    :delivery_date_id   ,null: false
+      t.integer    :cost               ,null: false
+      t.references :user               ,null: false, foreign_key: true
       t.timestamps
     end
   end
