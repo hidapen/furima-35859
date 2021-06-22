db8b0e8969d99f870ae02da42e0139fd9afe4237
hidapen <hidapen0630@gmail.com>
2021-06-22 12:59:31 +0900

    コントローラー設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,26 @@
 class OrdersController < ApplicationController
-  def index
-    @order = Order.new
-    @item = Item.find(params[:item_id])
+  before_action :authenticate_user!, except: :index
 
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
 end
