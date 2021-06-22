9e7e2a25beba75ac3036d522310da270aeeac6db
hidapen <hidapen0630@gmail.com>
2021-06-22 13:03:56 +0900

    単体テストコード

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-0000'}
+    prefecture_id { 3 }
+    municipality { '札幌市' }
+    house_number { '123-456' }
+    building_name { 'はなわ壮' }
+    phone_number { '09012345678' }
+    user_id { '2' }
+    item_id { '3' }
+  end
+end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '購入情報の保存' do
+    before do
+      @order_address = FactoryBot.build(:order_address)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+      it 'building_nameは空でも保存できること' do
+        @order_address.building_name = ''
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'numberが空だと保存できないこと' do
+      end
+      it 'exp_mothが空だと保存できないこと' do
+      end
+      it 'exp_yearが空だと保存できないこと' do
+      end
+      it 'cvcが空だと保存できないこと' do
+      end
+      it 'postal_codeが空だと保存できないこと' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal codeを入力してください")
+      end
+      it 'prefecture_idが空だと保存できないこと' do
+        @order_address.prefecture_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefectureを入力してください")
+      end
+      it 'municipalityが空だと保存できないこと' do
+        @order_address.municipality = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Municipalityを入力してください")
+      end
+      it 'house_numberが空だと保存できないこと' do
+        @order_address.house_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("House numberを入力してください")
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone numberを入力してください")
+      end
+      it 'userが紐付いていないと保存できないこと' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Userを入力してください")
+      end
+      it 'itemが紐付いていないと保存できないこと' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Itemを入力してください")
+      end
+      it 'postal_codeは「3桁ハイフン4桁」半角文字列でないと保存できないこと' do
+        @order_address.postal_code = '12345'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal codeはハイフンを含めてください")
+      end
+      it 'phone_numberが10桁以上11桁以内の半角数値でないと保存できないこと' do
+        @order_address.phone_number = '12345'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone numberは不正な値です")
+      end
+    end
+  end
+end
